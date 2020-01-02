04ceb75bb597e5842f370f4346d8d3123c2eab42
Pjeferson <paulojeferson34@gmail.com>
2020-01-02 17:33:02 -0300

    viewing react immutability

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -2,16 +2,42 @@ import React, { Component } from "react";
 
 class TechList extends Component {
   state = {
-    techs: ["Node.js", "ReactJS", "React Native"]
+    techs: ["Node.js", "ReactJS", "React Native"],
+    newTech: ""
+  };
+
+  handleInputChange = e => {
+    this.setState({
+      newTech: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ""
+    });
+    e.preventDefault();
   };
 
   render() {
     return (
-      <ul>
-        {this.state.techs.map(tech => (
-          <li key={tech}>{tech}</li>
-        ))}
-      </ul>
+      <>
+        <h1>{this.state.newTech}</h1>
+        <ul>
+          {this.state.techs.map(tech => (
+            <li key={tech}>{tech}</li>
+          ))}
+        </ul>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.newTech}
+            onChange={this.handleInputChange}
+          />
+          <input type="submit" value="Adicionar" />
+        </form>
+      </>
     );
   }
 }
