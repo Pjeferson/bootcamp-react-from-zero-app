04c88cdb79bb6ee3f812d64cd6cf1e4c0b0f7b53
Pjeferson <paulojeferson34@gmail.com>
2020-01-02 16:26:30 -0300

    creating first class component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React from "react";
 
 import "./App.css";
 
+import TechList from "./components/TechList";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <TechList />
+    </div>
+  );
 }
 
 export default App;
